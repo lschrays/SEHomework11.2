cce6a42c0769fd4b349abcd911a364d4198c4e8d
lschrays <84073594+lschrays@users.noreply.github.com>
2022-01-09 19:40:54 +0100

    Update Author.java

--- StaticAnalysisAutomationCode/Author.java
+++ StaticAnalysisAutomationCode/Author.java
@@ -4,7 +4,7 @@ package _package_1;
 public class Author {
 
 	public Author(String lastPart, String lastPart2, String vonPart, String firstPart, String jrPart) {
-		// TODO Auto-generated constructor stub test
+		// TODO Auto-generated constructor stub test2
 	}
 
 }
